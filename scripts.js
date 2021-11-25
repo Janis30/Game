a79d3b171661bda08296aae4848b816a0603e335
Janis30 <tsilvarodriguez@collegeforcreativestudies.edu>
2021-11-24 20:39:23 -0500

    -

--- scripts.js
+++ scripts.js
@@ -12,7 +12,6 @@ let bubbles = [];
 let score = 0;
 let speedMultiplier = 0;
 
-
 // Timers fro tracking our bubble creation
 const spawnTime = 1000;
 let currentTime = 10;
@@ -27,7 +26,9 @@ function createBubble(){
         y: 0,
         r: 25,
         speed: random (1,3 * score/200),
-        color: random(bubbleColor)
+        color: random(bubbleColor),
+        hit: false,
+        
     })
     lastCreationTime = new Date().getTime();
 }
@@ -37,6 +38,7 @@ let hero = {
     x: 200,
     y: 700,
     r: 50,
+    lives: 3,
 }
 
 
@@ -57,17 +59,20 @@ function draw (){
         text(score, 300, 50)
 
         lifeRect();
-    // heroDies();
+       
    
     for(let [index, bubble] of bubbles.entries()){
         fill (bubble.color)
         circle(bubble.x, bubble.y, bubble.r)
         bubble.y = bubble.y + bubble.speed
-        isHit(hero, bubble)
+        // isHit(hero, bubble)
+
+        bubble.hit = collideCircleCircle(bubble.x, bubble.y, bubble.r, hero.x, hero.y, hero.r)
+        bubbleHitHero(bubble, index)
         isOffScreen(bubble, index)
+
     }
 
-  
     currentTime = new Date().getTime();
     if (currentTime - lastCreationTime > spawnTime){
         createBubble();
@@ -75,7 +80,23 @@ function draw (){
 
     hero.x = constrain(hero.x, (hero.r/2), 400 - (hero.r/2));
 
-    console.log(lives)
+    console.log(hero.lives)
+    }
+}
+
+// Red bubble kill hero
+function bubbleHitHero(bubble, index){
+    if(bubble.hit == true){
+        bubbles.splice(index, 1)
+        
+
+        if(bubble.color == "red"){
+            console.log('hit')
+             hero.lives = hero.lives - 1
+        }
+        
+    }else{
+       
     }
 }
 
@@ -90,28 +111,28 @@ function keyPressed(){
 function isOffScreen(bubble, index){
     if(bubble.y > 800){
         bubbles.splice(index,1)
-        console.log(bubbles)
+        // console.log(bubbles)
         //score = score + 10
         speedMultiplier = 5
     }
 
 }
 
-function isHit(hero, bubble){
-    if(bubble.x > hero.x && bubble.x + bubble.r < hero.x + hero.r){
-        if(bubble.y + bubble.r > hero.y && bubble.y + bubble.r < hero.y + hero.r){
+// function isHit(hero, bubble){
+//     if(bubble.x > hero.x && bubble.x + bubble.r < hero.x + hero.r){
+//         if(bubble.y + bubble.r > hero.y && bubble.y + bubble.r < hero.y + hero.r){
             
-            if(bubble.color = "blue"){
-                console.log("hit!");
-                bubble.x = bubble.x +1;
-                score = score + 10;
-            }else if (bubble.color = "red"){
-                window.location.reload();
-            }
-        }
+//             if(bubble.color = "blue"){
+//                 console.log("hit!");
+//                 bubble.x = bubble.x +1;
+//                 score = score + 10;
+//             }else if (bubble.color = "red"){
+//                 window.location.reload();
+//             }
+//         }
     
-    }
-}
+//     }
+// }
 
 
 // show lifes of character 
